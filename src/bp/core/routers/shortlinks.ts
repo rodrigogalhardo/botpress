8cfd1d855187b457b616c8a5e09c362dc3e9144f
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2021-07-08 10:41:18 -0400

    Add more logs to shortlinks

--- src/bp/core/routers/shortlinks.ts
+++ src/bp/core/routers/shortlinks.ts
@@ -16,23 +16,30 @@ export class ShortLinksRouter extends CustomRouter {
   private setupRoutes(): void {
     this.router.get('/:name', (req, res) => {
       const name = req.params.name
+      this.logger.info(`[ShortLinksRouter] Received GET for name: ${name}`)
       let link = name && this.shortlinks.get(name)
+
+      this.logger.info(`[ShortLinksRouter] beginning link: ${link}`)
 
       if (!link) {
         return res.status(404).send(`Shortlink "${name}" not registered`)
       }
 
       const query = qs.stringify(req.query)
+      this.logger.info(`[ShortLinksRouter] query: ${query}`)
       if (query) {
         const hasQuery = /\?/g.test(link)
+        this.logger.info(`[ShortLinksRouter] hasQuery: ${hasQuery}`)
         link = link.concat(`${hasQuery ? '&' : '?'}${query}`)
       }
 
+      this.logger.info(`[ShortLinksRouter] final link: ${link}`)
       res.redirect(link)
     })
   }
 
   createShortLink(name: string, destination: string, params?: any) {
+    this.logger.info(`[ShortLinksRouter] createShortLink: ${name} ${destination} ${JSON.stringify(params)}`)
     name = name.toLocaleLowerCase()
 
     if (this.shortlinks.has(name)) {
